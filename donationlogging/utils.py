c87759104f1e7a69fe9d99451f45d1bc7012b8a3
crayyy_zee <zaidlastid20@gmail.com>
2021-11-21 02:34:47 +0500

    Revert "dono rewrite (1)"
    This reverts commit ef034f23408dfabe2198f34449ce925fec82a86e.

--- donationlogging/utils.py
+++ donationlogging/utils.py
@@ -1,5 +1,4 @@
 from redbot.core import commands
-from discord.ext.commands.view import StringView
 import re
 
 time_regex = re.compile(r"(?:(\d{1,5})(h|s|m|d))+?")
@@ -39,63 +38,3 @@ async def sortdict(argument, key_or_value="value"):
       final[i[0]] = i[1]
 
     return final
-  
-class flags(commands.Converter):
-    """
-    This is a custom flag parsing class made by me with help from skelmis (ethan) from menudocs."""
-    def __init__(self, *, delim=None, start=None):
-        self.delim = delim or " "
-        self.start = start or "--"
-
-    async def convert(self, ctx, argument):
-        x = True
-        argless = []
-        data = {None: []}
-        argument = argument.split(self.start)
-
-        if (length := len(argument)) == 1:
-            # No flags
-            argless.append(argument[0])
-            x = False  # Don't loop
-
-        i = 0
-        while x:
-            if i >= length:
-                # Have consumed all
-                break
-
-            if self.delim in argument[i]:
-                # Get the arg name minus start state
-                arg = argument[i].split(self.delim, 1)
-
-                if len(arg) == 1:
-                    # Arg has no value, so its argless
-                    # This still removes the start and delim however
-                    argless.append(arg)
-                    i += 1
-                    continue
-
-                arg_name = arg[0]
-                arg_value = arg[1].strip()
-
-                data[arg_name] = arg_value
-
-            else:
-                argless.append(argument[i])
-
-            i += 1
-
-        # Time to manipulate argless
-        # into the same expected string pattern
-        # as dpy's argparsing
-        for arg in argless:
-            view = StringView(arg)
-            while not view.eof:
-                word = view.get_quoted_word()
-                data[None].append(word)
-                view.skip_ws()
-
-        if not bool(data[None]):
-            data.pop(None)
-
-        return data
