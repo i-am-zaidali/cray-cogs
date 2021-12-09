f40b8ffe88190f23aedfdf07b04b19216570c1ee
crayyy_zee <65814383+i-am-zaidali@users.noreply.github.com>
2021-12-09 14:50:21 +0500

    [giveaways] removing the reaction would be appreciated

--- giveaways/events.py
+++ giveaways/events.py
@@ -75,6 +75,7 @@ class main(commands.Cog):
             if str(payload.emoji) == (emoji := (ind := data[e.index(payload.message_id)]).emoji):
                 message = await ind.get_message()
                 if not ind.donor_can_join and payload.member == ind.donor:
+                    await message.remove_reaction(emoji, payload.member)
                     embed = discord.Embed(
                         title="Entry Invalidated!",
                         description=f"Your entry for [this]({message.jump_url}) giveaway has been removed.\n"
