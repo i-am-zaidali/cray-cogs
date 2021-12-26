ff516194929a7c13985de915d09be73ffd10fdf1
crayyy_zee <zaidlastid20@gmail.com>
2021-12-26 20:29:21 +0500

    i was bored ok

---
+++ tickchanger/util.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext.commands.converter import EmojiConverter as ec
+from emoji import UNICODE_EMOJI_ENGLISH
+from redbot.core import commands
+from redbot.core.bot import Red
+
+old_tick = commands.context.TICK
+
+old_get_context = Red.get_context
+
+
+class FakeContext(commands.Context):
+    tick_emoji = None
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def tick(self) -> bool:
+        """Add a tick reaction to the command message.
+
+        Returns
+        -------
+        bool
+            :code:`True` if adding the reaction succeeded.
+
+        """
+        try:
+            await self.message.add_reaction(self.tick_emoji)
+        except discord.HTTPException:
+            return False
+        else:
+            return True
+
+
+class EmojiConverter(ec):
+    async def convert(self, ctx, argument):
+        argument = argument.strip()
+        if not argument in UNICODE_EMOJI_ENGLISH.keys():
+            return await super().convert(ctx, argument)
+        return argument
