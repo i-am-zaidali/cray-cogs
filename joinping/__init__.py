e6d823cc6d47a4c44a3a1965fead879bcb97b4bd
crayyy_zee <zaidlastid20@gmail.com>
2022-01-17 20:58:29 +0500

    [joinping] yes

---
+++ joinping/__init__.py
@@ -0,0 +1,13 @@
+import json
+from pathlib import Path
+
+from .main import JoinPing
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+
+
+async def setup(bot):
+    cog = JoinPing(bot)
+    await cog._build_cache()
+    bot.add_cog(cog)
