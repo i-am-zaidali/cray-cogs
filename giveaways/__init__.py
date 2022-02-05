f40acbcd9e57c9957f64190db19d79cdaa7a4118
crayyy_zee <zaidlastid20@gmail.com>
2022-02-05 21:42:58 +0500

    [giveaways] info.json

--- giveaways/__init__.py
+++ giveaways/__init__.py
@@ -1,6 +1,12 @@
 from .gset import Gset
 
+import json
+from pathlib import Path
+
+with open(Path(__file__).parent / "info.json") as fp:
+    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 async def setup(bot):
-    cog = await Gset.initialize(bot)
+    cog = Gset(bot)
     bot.add_cog(cog)
+    bot.loop.create_task(cog.initialize())
