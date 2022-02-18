4546889ee2307e4b3477fb1fae1d6a91977467b1
crayyy_zee <zaidlastid20@gmail.com>
2022-02-18 22:27:13 +0500

    [tickchanger] stop modifying context altogether

--- tickchanger/util.py
+++ tickchanger/util.py
@@ -1,42 +1,6 @@
 import discord
 from discord.ext.commands.converter import EmojiConverter as ec
-from discord.ext.commands.converter import PartialEmojiConverter
 from emoji import UNICODE_EMOJI_ENGLISH
-from redbot.core import commands
-from redbot.core.bot import Red
-
-old_tick = commands.context.TICK
-
-old_get_context = Red.get_context
-
-
-class FakeContext(commands.Context):
-    tick_emoji = None
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-    async def tick(self, *, message: str = None) -> bool:
-        """Add a tick reaction to the command message.
-
-        Keyword Arguments
-        -----------------
-        message : str, optional
-            The message to send if adding the reaction doesn't succeed.
-
-        Returns
-        -------
-        bool
-            :code:`True` if adding the reaction succeeded.
-
-        """
-
-        emoji = (
-            self.tick_emoji if self.channel.permissions_for(self.me).external_emojis else old_tick
-        )
-
-        return await self.react_quietly(emoji, message=message)
-
 
 class EmojiConverter(ec):
     async def convert(self, ctx, argument):
