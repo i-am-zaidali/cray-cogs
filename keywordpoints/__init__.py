509af5a8fe07c4ed6b3f7487a4ea52af35a5e14a
crayyy_zee <zaidlastid20@gmail.com>
2022-09-07 20:23:56 +0500

    [kwp] fix # 1

--- keywordpoints/__init__.py
+++ keywordpoints/__init__.py
@@ -2,7 +2,7 @@ from .main import KeyWordPoints
 
 from redbot.core.utils import get_end_user_data_statement
 
-__red_end_user_data_statement__ = get_end_user_data_statement()
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot):
     cog = KeyWordPoints(bot)
