cfc5499569caa39b6a6446f2a7ef48674716145d
crayyy_zee <zaidlastid20@gmail.com>
2022-10-09 21:10:34 +0500

    Add listnotes command to `Notes` cog This PR adds a command that basically just sends an embed listing how many users have how many notes. Resolves #35

--- notes/notes.py
+++ notes/notes.py
@@ -10,6 +10,7 @@ from redbot.core import Config, commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import humanize_list
 from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
+from tabulate import tabulate
 
 from .models import NoteType, UserNote
 
@@ -235,6 +236,22 @@ class Notes(commands.Cog):
         # await paginator.start()
 
         await menu(ctx, embeds, DEFAULT_CONTROLS)
+        
+    @commands.command(name="listnotes")
+    @commands.mod_or_permissions(manage_messages=True)
+    async def listnotes(self, ctx: commands.Context):
+        notes = self._get_notes(ctx.guild)
+        
+        data = sorted([(i, u, len(notes)) for i, (user, notes) in enumerate(notes.items()) if (u := ctx.guild.get_member(user))], key=lambda x: x[2], reverse=True)
+        
+        tabbed = tabulate(data, headers=["User", "Notes"], tablefmt="rst")
+        
+        return await ctx.send(
+            embed=discord.Embed(
+                title="Notes for this server",
+                description=f"```rst\n{tabbed}```",
+            )
+        )
 
     @commands.command()
     @commands.mod_or_permissions(manage_messages=True)
