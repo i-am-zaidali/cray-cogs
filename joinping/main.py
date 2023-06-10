ea7c0252ee801900c0850c80f83ff4aed9ddcc89
crayyy_zee <65814383+i-am-zaidali@users.noreply.github.com>
2023-06-10 20:30:54 +0500

    Forgot to add variable getter blocks

--- joinping/main.py
+++ joinping/main.py
@@ -54,7 +54,7 @@ class JoinPing(commands.Cog):
             return
 
         message = guild_data.get("ping_message", "")
-        engine = tse.AsyncInterpreter([tse.EmbedBlock()])
+        engine = tse.AsyncInterpreter([tse.EmbedBlock(), tse.LooseVariableGetterBlock(), tse.StrictVariableGetterBlock(),])
         resp = await engine.process(
             message,
             seed_variables={
