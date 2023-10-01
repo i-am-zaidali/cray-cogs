fe8ff08be3b0bd91ac219257a2e41f6c52073f73
{{ github.actor }} <i-am-zaidali@users.noreply.github.com>
2023-10-01 18:22:54 +0000

    Style Reformatting

--- timer/timers.py
+++ timer/timers.py
@@ -1,7 +1,7 @@
 import asyncio
 import itertools
 import logging
-from typing import Dict, List, Iterable, Callable, Any, Optional, TypeVar
+from typing import Any, Callable, Dict, Iterable, List, Optional, TypeVar
 
 import discord
 from discord.ext import tasks
